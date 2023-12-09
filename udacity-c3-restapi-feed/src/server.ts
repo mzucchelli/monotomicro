7366570bcb0b5960ea203b9182c7ff55b5d479aa
Marc <marc@marc>
2023-12-09 12:02:45 -0500

    updates

--- udacity-c3-restapi-feed/src/server.ts
+++ udacity-c3-restapi-feed/src/server.ts
@@ -20,6 +20,7 @@ const c = config.dev;
 
   //CORS Should be restricted
   app.use(function(req, res, next) {
+    console.log("allowing origin:", c.url);
     res.header("Access-Control-Allow-Origin", c.url);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
