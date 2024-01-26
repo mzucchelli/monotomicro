000f5d63b6731d38b85d0ebb2d47cd3863930056
Marc <marc@marc>
2024-01-25 19:04:06 -0500

    forgot to quote a string

--- udacity-c3-frontend/src/environments/environment.ts
+++ udacity-c3-frontend/src/environments/environment.ts
@@ -2,7 +2,7 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-let apiHost = process.env.API_HOST || localhost;
+let apiHost = process.env.API_HOST || 'localhost';
 
 export const environment = {
   production: false,
